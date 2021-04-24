9d1992df33a056ac919c2bd4b1d43bcd28e5d1a7
Jennifer Pazos <jmp521@gmail.com>
2021-04-24 13:54:42 -0500

    updated timeslot controller and model

---
+++ app/controllers/timeslots_controller.rb
@@ -0,0 +1,50 @@
+class TimeslotsController < ApplicationController
+    before_action :set_timeslot, only: [:show, :update, :destroy]
+    def index
+        @timeslot = Timeslot.all
+        render json: @timeslot
+    end
+
+    def show
+        if @timeslot
+            render json: @timeslot, include: :user
+        end
+    end
+
+    def update
+        if @timeslot
+            @timeslot.update(timeslot_params)
+            render json: @timeslot
+        else
+            render json: {error: "Could not update timeslot"}
+        end
+    end
+
+    def create
+        @timeslot = Timeslot.new(timeslot_params)
+        if @timeslot
+            render json: @timeslot, include: :user
+        else
+            render json: {error: "Timeslot was not created"}
+        end
+    end
+
+    def destroy
+        if @timeslot
+            @timeslot.destroy
+            render json: @timeslot
+        end
+    end
+
+    private
+
+    def timeslot_params
+        params.require(:timeslot).permit(:activity, :id, :taken, :booked_time, :booked_email)
+    end
+
+
+    def set_timeslot
+        @timeslot = Timeslot.find_by(id: params[:id])
+    end
+
+end
